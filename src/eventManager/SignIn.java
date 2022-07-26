6c91acf3f5d743091104000e8edaff48e6ef896f
Steven <85467655+cubespot@users.noreply.github.com>
2022-07-26 11:48:35 -0700

    Create SignIn.java

---
+++ src/eventManager/SignIn.java
@@ -0,0 +1,80 @@
+package eventManager;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SignIn extends JFrame implements ActionListener {
+    private JLabel password1, label;
+    private JTextField username, password;
+    private JButton logButton, signButton;
+
+    public SignIn() {
+        JPanel panel = new JPanel();
+        JFrame frame = new JFrame();
+        setTitle("Event Manager");
+        setBounds(250, 150, 750, 500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.add(panel);
+
+        panel.setLayout(null);
+
+        label = new JLabel("Name");
+        label.setFont(new Font("Arial", Font.PLAIN, 15));
+        label.setBounds(250, 158, 70, 20);
+        panel.add(label);
+        username = new JTextField();
+        username.setFont(new Font("Arial", Font.PLAIN, 15));
+        username.setBounds(250, 175, 193, 28);
+        panel.add(username);
+        password1 = new JLabel("Password");
+        password1.setFont(new Font("Arial", Font.PLAIN, 15));
+        password1.setBounds(250, 205, 70, 20);
+        panel.add(password1);
+        password = new JTextField();
+        password.setFont(new Font("Arial", Font.PLAIN, 15));
+        password.setLocation(250, 225);
+        password.setSize(193, 28);
+        panel.add(password);
+        logButton = new JButton("Login");
+        logButton.setFont(new Font("Arial", Font.PLAIN, 15));
+        logButton.setBounds(250, 260, 90, 25);
+        logButton.addActionListener(this);
+        panel.add(logButton);
+        signButton = new JButton("Signup");
+        signButton.setFont(new Font("Arial", Font.PLAIN, 15));
+        signButton.setBounds(350, 260, 90, 25);
+        signButton.addActionListener(this);
+        panel.add(signButton);
+
+        add(panel);
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        String data = username.getText();
+        String data2 = password.getText();
+
+        Map<String, String> map = new HashMap<>();
+        if (e.getSource() == logButton) {
+            for (int i = 0; i < map.size(); i++) {
+                if (map.get(data2) == data) {
+
+                }
+            }
+        } else if (e.getSource() == signButton) {
+            map.put(data2, data);
+            String def = "";
+            username.setText(def);
+            password.setText(def);
+        }
+    }
+}
+
+class Tester {
+    public static void main(String[] args) throws Exception {
+        SignIn s = new SignIn();
+    }
+}
