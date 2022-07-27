96f06300dd7b4dace1d2efd6537edabacc9311f9
Steven <85467655+cubespot@users.noreply.github.com>
2022-07-26 23:19:17 -0700

    Update SignIn.java

--- src/eventManager/SignIn.java
+++ src/eventManager/SignIn.java
@@ -7,11 +7,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SignIn extends JFrame implements ActionListener {
-    private JLabel password1, label;
-    private JTextField username, password;
+    private JLabel title, password1, label;
+    private JTextField username;
+    private JPasswordField password;
     private JButton logButton, signButton, backButton;
     private JFrame frame;
 
+    /**
+     * Constructor method for instance variables
+     */
     public SignIn() {
         JPanel panel = new JPanel();
         frame = new JFrame();
@@ -22,73 +26,96 @@ public class SignIn extends JFrame implements ActionListener {
 
         panel.setLayout(null);
 
-        label = new JLabel("Name");
+        title = new JLabel("Login");
+        title.setFont(new Font("Arial", Font.BOLD, 20));
+        title.setBounds(320, 120, 70, 30);
+        panel.add(title);
+
+        label = new JLabel("User");
         label.setFont(new Font("Arial", Font.PLAIN, 15));
         label.setBounds(250, 158, 70, 20);
         panel.add(label);
+
         username = new JTextField();
         username.setFont(new Font("Arial", Font.PLAIN, 15));
         username.setBounds(250, 175, 193, 28);
         panel.add(username);
+
         password1 = new JLabel("Password");
         password1.setFont(new Font("Arial", Font.PLAIN, 15));
         password1.setBounds(250, 205, 70, 20);
         panel.add(password1);
-        password = new JTextField();
+
+        password = new JPasswordField();
         password.setFont(new Font("Arial", Font.PLAIN, 15));
-        password.setLocation(250, 225);
-        password.setSize(193, 28);
+        password.setBounds(250, 225, 193, 28);
         panel.add(password);
+
         logButton = new JButton("Login");
         logButton.setFont(new Font("Arial", Font.PLAIN, 15));
         logButton.setBounds(250, 260, 90, 25);
         logButton.addActionListener(this);
         panel.add(logButton);
+
         signButton = new JButton("Signup");
         signButton.setFont(new Font("Arial", Font.PLAIN, 15));
         signButton.setBounds(350, 260, 90, 25);
         signButton.addActionListener(this);
         panel.add(signButton);
+
         backButton = new JButton("Back to Main Menu");
         backButton.setFont(new Font("Arial", Font.PLAIN, 15));
         backButton.setBounds(250, 300, 190, 25);
         backButton.addActionListener(this);
         panel.add(backButton);
-        
+
         add(panel);
         setVisible(true);
     }
 
+    /**
+     * Acts based on user actions
+     * 
+     * @param e action performed
+     */
     public void actionPerformed(ActionEvent e) {
         String data = username.getText();
-        String data2 = password.getText();
+        String data2 = String.valueOf(password.getPassword());
 
         Map<String, String> map = new HashMap<>();
         if (e.getSource() == logButton) {
-            for (int i = 0; i < map.size(); i++) {
-                if (map.get(data2) == data && !data.equals("") && !data2.equals(""))
-                	getBooth();
+            if (map.containsKey(data2)) {
+                if (map.get(data2).equals(data) && !data.equals("") && !data2.equals(""))
+                    getBooth();
             }
         } else if (e.getSource() == signButton) {
             map.put(data2, data);
             if (!data.equals("") && !data2.equals(""))
-            	getBooth();
+                getBooth();
             String def = "";
             username.setText(def);
             password.setText(def);
-        }
-        else if (e.getSource() == backButton) {
+        } else if (e.getSource() == backButton) {
             frame.setVisible(false);
-    		OpeningPage f = new OpeningPage();
+            OpeningPage f = new OpeningPage();
         }
     }
-    
+
+    /**
+     * Getting booth panel
+     */
     public static void getBooth() {
-    	BoothAddPage b = new BoothAddPage();
+        BoothAddPage b = new BoothAddPage();
     }
 }
 
 class Tester {
+    /**
+     * Main method
+     * 
+     * @param args console input
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         SignIn s = new SignIn();
     }
