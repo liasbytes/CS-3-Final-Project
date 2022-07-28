f8157703c1c64ec4ba456bee704e07d8ee664348
Steven <85467655+cubespot@users.noreply.github.com>
2022-07-27 17:59:27 -0700

    Create BoothReading.java

---
+++ src/BoothReading.java
@@ -0,0 +1,41 @@
+
+import java.util.*;
+
+import eventManager.Booth;
+import eventManager.Booth.BoothType;
+
+import java.io.*;
+
+public class BoothReading {
+
+    /**
+     * Main method which calls readFile method
+     * 
+     * @param args console input
+     */
+    public static void main(String[] args) {
+        readFile("booth-data.txt");
+    }
+
+    /**
+     * Reads the file and adds booth data into an object, which is added into an
+     * arraylist
+     * 
+     * @param fileName name or path of file
+     * @return arraylist storing Booth objects
+     */
+    public static ArrayList<Booth> readFile(String fileName) {
+        Scanner scan = null;
+        try {
+            scan = new Scanner(new File(fileName));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        ArrayList<Booth> boothData = new ArrayList<>();
+        while (scan.hasNext()) {
+            boothData.add(new Booth(scan.next(), scan.next(), scan.nextInt(), BoothType.valueOf(scan.next()),
+                    scan.nextInt()));
+        }
+        return boothData;
+    }
+}
