33e33c74bcb54da15100d1fed20233d9a6ccd9ea
lilyz25 <109823790+lilyz25@users.noreply.github.com>
2022-07-28 12:52:49 -0700

    Organizer Methods Update

--- src/eventManager/Organizer.java
+++ src/eventManager/Organizer.java
@@ -3,16 +3,29 @@ package eventManager;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 
 class Organizer
 	implements ActionListener {
 
 	// Components
-	private static JPanel panel, deletePanel, buttonPane;
+	private static JPanel panel, deletePanel;
 	private static JLabel title, label;
-	private JButton deleteBooth, addBooth;
-	private static JCheckBox booth;	
+	private JButton deleteBooth, addBooth, myDelete;
 	private JFrame frame;
+	private HashMap<Integer, Account> accounts;
+	private HashMap<String, Booth> tempMap;
+	private int currentID;
+	private ArrayList<Booth> booths;
+	private String boothID;
 	
 	/**
 	 * constructor to initialize components
@@ -20,6 +33,8 @@ class Organizer
 	public Organizer(JFrame frame)
 	{
 		this.frame = frame;
+		booths = BoothReading.readFile("booth-data.txt");
+		tempMap = new HashMap<String, Booth>();
 		
 		deletePanel = new JPanel(new GridBagLayout());
 		deletePanel.setLayout(new javax.swing.BoxLayout(deletePanel, javax.swing.BoxLayout.Y_AXIS));
@@ -52,36 +67,163 @@ class Organizer
 		deleteBooth.addActionListener(this);
 		panel.add(deleteBooth);
 		
-		addBooth = new JButton("Add a booth");
+		addBooth = new JButton("Add an unavailable booth");
 		addBooth.setFont(new Font("Serif", Font.BOLD, 17));
 		addBooth.addActionListener(this);
 		panel.add(addBooth);
-
-		/*
-		buttonPane = new JPanel();
-		buttonPane.setLayout(new FlowLayout());
-		buttonPane.setBorder(BorderFactory.createEmptyBorder(0, 0, 50, 50));
-		buttonPane.add(deleteBooth);
-		buttonPane.add(Box.createRigidArea(new Dimension(50, 0)));
-		buttonPane.add(addBooth);
-		panel.add(buttonPane, BorderLayout.CENTER);
-		*/
 		
 		frame.add(panel);
 		frame.setVisible(true);
 	}
 
-	public static void deleteBooth(Booth[][] data) {
-		for (int i = 0; i < data.length; i++) {
-			for (int j = 0; j < data[i].length; j++) {
-			String tempName = data[i][j].getName();
-			System.out.println(tempName);
-			booth = new JCheckBox(tempName);
-			booth.setFont(new Font("Serif", Font.PLAIN, 17));
-			booth.setBorder(BorderFactory.createEmptyBorder(10,15,0,0));
-			deletePanel.add(booth);
+	public String displayBooths() {
+		
+		for (int i = 0; i < booths.size(); i++) {
+			String tempName = booths.get(i).getName();
+			tempMap.put(tempName, booths.get(i));
 			}
-		}
+		
+		ArrayList<String> tempBooths = new ArrayList<String>();
+		for (Booth b : booths)
+			tempBooths.add(b.getName());
+		
+		String[] myBooths = tempBooths.toArray(new String[0]);
+	        JComboBox<String> jComboBox = new JComboBox<>(myBooths);
+	        jComboBox.setBounds(80, 50, 140, 20);
+
+	        myDelete = new JButton("Delete");
+	        myDelete.setBounds(100, 100, 90, 20);
+	        myDelete.addActionListener(this);
+
+	        JLabel jLabel = new JLabel();
+	        jLabel.setBounds(90, 100, 400, 100);
+
+	        deletePanel.add(myDelete);
+	        deletePanel.add(jComboBox);
+	        deletePanel.add(jLabel);
+	        
+	        deletePanel.setLayout(null);
+	        deletePanel.setSize(350, 250);
+	        deletePanel.setVisible(true);
+	        
+	        String boothID = (String) jComboBox.getSelectedItem();
+	        return boothID;
+	}
+	
+	public void deleteBooth(Booth booth) {
+		 int input = JOptionPane.showConfirmDialog(null, 
+	                "Are you sure you want to continue?", "Select an Option...",JOptionPane.YES_NO_CANCEL_OPTION);
+		 if (input == 0) {
+			 // yes
+			 
+			 File tbm = new File("booth-data.txt");
+			 String oldContent = "";
+			 String oldString = "";
+			 FileWriter fw = null;
+			 BufferedReader reader = null;
+			 boolean found = false;
+			 
+				try {
+					reader = new BufferedReader(new FileReader(tbm));
+		        	
+					String line = reader.readLine();
+					while (line != null){
+						if (booth != null && line.equals(String.valueOf(booth.getBoothID()))) {
+				        	for (int i = 0; i < 5; i++) {
+					        	oldString = oldString.concat(line);
+					        	oldString = oldString.concat(System.lineSeparator());
+				        		line = reader.readLine();
+				        	}
+				        	found = true;
+				        	System.out.println(oldString);
+				        }
+					        oldContent = oldContent + line + System.lineSeparator();
+					        line = reader.readLine();
+					        
+					}
+					reader.close();
+					if (found == true) {
+						fw = new FileWriter(tbm, false);
+						readAccounts(SignIn.BOOTH_ACCOUNT_PATH);
+						accounts.remove(booth.getBoothID());
+						writeAccounts(SignIn.BOOTH_ACCOUNT_PATH);
+						String newContent = oldContent.replaceAll(oldString, "");
+						System.out.println(newContent);
+						fw.write(newContent);
+						JOptionPane.showMessageDialog(null, "Your booth has been deleted.");
+						fw.close();
+						disableComponents();
+						// go to home page
+						OpeningPage o = new OpeningPage(this.frame);
+						
+					} else {
+				        JOptionPane.showMessageDialog(null, "Something went wrong. Try again later.");
+				        fw.close();
+				        disableComponents();
+						OpeningPage o = new OpeningPage(this.frame);
+					}
+					
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}			
+			 
+		 } else if (input == 1) {
+			 // no
+		 } else if (input == 2) {
+			 // cancel
+		 }
+	}
+	
+	private void readAccounts(String filePath) {
+    	accounts = new HashMap<>();
+    	Scanner scan = null;
+    	try {
+    		scan = new Scanner(new File(filePath));
+    		currentID = scan.nextInt();
+    		scan.nextLine();
+    		while(scan.hasNextLine()) {
+    			String username = scan.nextLine();
+        		String password = scan.nextLine();
+        		int boothID = Integer.parseInt(scan.nextLine());
+        		accounts.put(boothID, new Account(username,password,boothID));
+    		}
+    		scan.close();
+    	} catch (FileNotFoundException e) {
+    		FileWriter fw = null;
+    		try {
+    			fw = new FileWriter(new File(filePath));
+    			fw.write("");
+    			fw.close();
+    		} catch (IOException e1) {
+    			e1.printStackTrace();
+    		}
+    	}
+    }
+    
+    private void writeAccounts(String filePath) {
+    	FileWriter fw = null;
+    	try {
+    		fw = new FileWriter(new File(filePath), false);	
+    		fw.write(String.valueOf(currentID));
+    		fw.write(String.format("%n"));
+    		for (Integer ID : accounts.keySet()) {
+    			String username = accounts.get(ID).username;
+    			String password = accounts.get(ID).password;
+    			fw.write(username);
+    			fw.write(String.format("%n"));
+    			fw.write(password);
+    			fw.write(String.format("%n"));
+    			fw.write(String.valueOf(ID));
+    			fw.write(String.format("%n"));
+    		}
+    		fw.close();
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+    }
+    
+	private static void disableComponents() {
+		deletePanel.setVisible(false);
 	}
 	
 	/**
@@ -90,14 +232,21 @@ class Organizer
 	 */
 	public void actionPerformed(ActionEvent e)
 	{
-	
+		//delete a booth
+		if (e.getSource() == myDelete) {
+			System.out.println("Test");
+	        deleteBooth(tempMap.get(boothID));
+		}
 		if (e.getSource() == deleteBooth) {
 			frame.add(deletePanel);
 			panel.setVisible(false);
-			deleteBooth(Test.getTestBooths());
+			boothID = displayBooths();
+			
+			
+		//add unavailable booth
 		} else if (e.getSource() == addBooth) {
 			panel.setVisible(false);
-			BoothAddPage b = new BoothAddPage(1, this.frame);
+			//data[x][x]new Booth(); to create new unavailable booth TODO position ask for user input
 		}
 	}
 }
